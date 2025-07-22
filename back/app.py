73941b4f4258e8a23f0b80612feca7796be0bd6a
Miguel A Barbosa M <barbosamartinezmiguelangel1@gmail.com>
2025-07-22 13:01:32 -0500

    Ajuste api y creación .yaml

--- back/app.py
+++ back/app.py
@@ -37,5 +37,6 @@ def predict():
 
     return jsonify({'Costos_MillonesCOP': round(float(prediccion[0]), 3)})
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
